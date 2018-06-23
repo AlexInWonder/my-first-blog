e47d05ddc6bb04ac420a851fb374f1bb00e7a340
AlexInWonder <timohinalexa@gmail.com>
2018-06-23 18:26:36 +0300

    Initiall commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#07cnov^(@4ug_s1v6)ay9z^h^(8tgsz3agirzm0gjvtbztz9n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','alexinwonder.pythonanywhere.com']
 
 
 # Application definition
